0b2e64c93d4f87b58b81385fd6b1f6f076b36ea9
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-09-24 09:15:15 -0300

    corrigindo um erro

--- src/app/contexts/ThemeContext.tsx
+++ src/app/contexts/ThemeContext.tsx
@@ -1,25 +1,32 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 const STORAGE_KEY = 'themeContextKey';
-type ThemeContext = {
+type ThemeContextType = {
     theme: string;
-    setTheme: (newTheme: string) => void
-}
+    setTheme: (newTheme: string) => void;
+};
 
-export const ThemeContext = createContext<ThemeContext | null>(null);
+export const ThemeContext = createContext<ThemeContextType | null>(null);
 
-export const ThemeProvider = ({children}: {children: ReactNode}) => {
-    const [theme, setTheme] = useState(
-        localStorage.getItem(STORAGE_KEY) || 'light'
-    );
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState('light');
+    
+    useEffect(() => {
+        const savedTheme = localStorage.getItem(STORAGE_KEY);
+        if (savedTheme) {
+            setTheme(savedTheme);
+        }
+    }, []);
 
     useEffect(() => {
         localStorage.setItem(STORAGE_KEY, theme);
-    }, [theme])
+    }, [theme]);
 
     return (
-        <ThemeContext.Provider value={{theme, setTheme}}>{children}</ThemeContext.Provider>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
     );
-}
+};
 
 export const useTheme = () => useContext(ThemeContext);
